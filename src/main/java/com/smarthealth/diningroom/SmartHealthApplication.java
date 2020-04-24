106ec19a78fe66ab52bbddc2f5de06b67d20b1cd
songlingqq <songlingsl@163.com>
2020-04-24 19:15:35 +0800

    Initial commit

---
+++ src/main/java/com/smarthealth/diningroom/SmartHealthApplication.java
@@ -0,0 +1,13 @@
+package com.smarthealth.diningroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHealthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartHealthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smarthealth/diningroom/SmartHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarthealth.diningroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHealthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
