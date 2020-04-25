b19fbfc754a2b06ebc0f6d742c2dc408759c0447
songlingqq <songlingsl@163.com>
2020-04-25 15:25:31 +0800

    提交

--- src/main/java/com/smarthealth/diningroom/SmartHealthApplication.java
+++ src/main/java/com/smarthealth/diningroom/SmartHealthApplication.java
@@ -1,13 +1,19 @@
 package com.smarthealth.diningroom;
 
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
+@MapperScan("com.smarthealth.diningroom.mapper")
 public class SmartHealthApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(SmartHealthApplication.class, args);
+        log.info("智慧健康启动");
     }
 
 }
