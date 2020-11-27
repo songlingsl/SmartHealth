c9927290801101bff2ed32cfc9d7c29ba73c6fff
songlingqq <songlingsl@163.com>
2020-11-27 20:08:49 +0800

    提交

--- src/main/java/com/smarthealth/diningroom/entity/Songling.java
+++ src/main/java/com/smarthealth/diningroom/entity/Songling.java
@@ -9,7 +9,8 @@ import java.io.Serializable;
 public class Songling implements Serializable {
     private static final long serialVersionUID = -3100715851080733340L;
     //@TableId(type = IdType.ASSIGN_ID)  全局已经配置雪花算法
-    private Long id;
+    private Long id=123456L;
+    private Long pId=456789L;
     private String name;
     private Integer number;
     @TableField(exist = false)//没有对应字段标识
